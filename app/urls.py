b8c6756cfe9b86eab184e9b7ecf0a72229fb7a44
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-01 10:01:08 +0300

    Implement home page

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from app.views import TaskListView
 
 urlpatterns = [
-
+    path('', TaskListView.as_view(), name='task-list'),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from app.models import Task
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    paginate_by = 5

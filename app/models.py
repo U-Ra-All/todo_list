2f3bccf1f00d350286775c518689f2ad25c37f9c
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-03 17:11:01 +0300

    Add sorting for Tag model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
